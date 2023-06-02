940d967e997de4bc2210248ca3800f1682213859
mjmohsinjaved <115790456+mjmohsinjaved@users.noreply.github.com>
2023-06-02 16:49:47 +0500

    Advance pagination helpers for server side paging
    Contains interfaces and classes to paginate data, which also return URI as a parameter in response to send call for next and previous pages

---
+++ PaginationHelpers/IPaginationHelper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Filter;
+using SharedLibrary.Services.ServiceUri;
+using SharedLibrary.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationHelpers
+{
+    public interface IPaginationHelper
+    {
+        Task<PagedResponse<List<T>>> CreatePagingAsync<T>(IQueryable<T> source, int pageIndex, int pageSize, string route);
+        Task<PagedResponse<List<TDestination>>> CreatePagingAsync<TSource, TDestination>(IQueryable<TSource> source, int pageIndex, int pageSize, string route);
+        PagedResponse<List<T>> CreatePagedReponse<T>(List<T> pagedData, PaginationFilter validFilter, int totalRecords, string route);
+    }
+}
---
+++ PaginationHelpers/PaginationHelper.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PaginationHelpers;
+using UriService;
+using SharedLibrary.Wrappers;
+namespace PaginationHelpers
+{
+    public class PaginationHelper : IPaginationHelper
+    {
+        private readonly IMapper _mapper;
+        private readonly IUriService _uriService;
+        public PaginationHelper(IMapper mapper, IUriService uriService){
+            _mapper = mapper;
+            _uriService = uriService;   
+        }
+        public  PagedResponse<List<T>> CreatePagedReponse<T>(List<T> pagedData, PaginationFilter paginationModel, int totalRecords,string route)
+        {
+            var respose = new PagedResponse<List<T>>(pagedData, paginationModel.PageNumber, paginationModel.PageSize);
+            var totalPages = ((double)totalRecords / (double)paginationModel.PageSize);
+            int roundedTotalPages = Convert.ToInt32(Math.Ceiling(totalPages));
+            respose.NextPage =
+                paginationModel.PageNumber >= 1 && paginationModel.PageNumber < roundedTotalPages
+                ? _uriService.GetPageUri(new PaginationFilter(paginationModel.PageNumber + 1, paginationModel.PageSize), route)
+                : null;
+            respose.PreviousPage =
+                paginationModel.PageNumber - 1 >= 1 && paginationModel.PageNumber <= roundedTotalPages
+                ? _uriService.GetPageUri(new PaginationFilter(paginationModel.PageNumber - 1, paginationModel.PageSize), route)
+                : null;
+            respose.FirstPage = _uriService.GetPageUri(new PaginationFilter(1, paginationModel.PageSize), route);
+            respose.LastPage = _uriService.GetPageUri(new PaginationFilter(roundedTotalPages, paginationModel.PageSize), route);
+            respose.TotalPages = roundedTotalPages;
+            respose.TotalRecords = totalRecords;
+            return respose;
+        }
+        public  async Task<PagedResponse<List<T>>> CreatePagingAsync<T>(IQueryable<T> source, int pageIndex, int pageSize,string route)
+        {
+            var filter = new PaginationFilter(pageIndex, pageSize);
+            var count = await source.CountAsync();
+            var items = await source.Skip((filter.PageNumber- 1) * filter.PageSize).Take(filter.PageSize).ToListAsync();
+            return CreatePagedReponse(items, filter, count,  route
+            );
+        }
+        public  async Task<PagedResponse<List<TDestination>>> CreatePagingAsync<TSource,TDestination>(IQueryable<TSource> source, int pageIndex, int pageSize, string route)
+        {
+            var filter = new PaginationFilter(pageIndex, pageSize);
+            var count = await source.CountAsync();
+            var items = await source.Skip((filter.PageNumber - 1) * filter.PageSize).Take(filter.PageSize).ToListAsync();
+            var data = (List<TDestination>)_mapper.Map(items, items.GetType(), typeof(List<TDestination>));
+            return CreatePagedReponse(data,filter,count, route);
+        }
+    }
+    
+}
